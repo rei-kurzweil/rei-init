819fb25a857f30559b9090eb0e5e7ec5bb0b2245
🧃hyper リリー <ultr7a@gmail.com>
2025-07-13 06:02:01 -0400

    add optional system-preference-aware light & dark themes 🦉OwO🌒✨

--- packages/ui/tsup.config.ts
+++ packages/ui/tsup.config.ts
@@ -2,8 +2,13 @@ import { defineConfig } from 'tsup';
 
 export default defineConfig({
     entry: ['src/index.ts'],
-    format: ['esm'], // add 'cjs' if needed
+    format: ['esm'],
     dts: true,
     sourcemap: true,
     clean: true,
+    outExtension({ format }) {
+        return {
+            js: format === 'esm' ? '.mjs' : '.js'
+        }
+    }
 });
